f8f0cda542ae3dd41c62013793674c5d05405bc3
John P Considine <jconsidi@nd.edu>
2017-11-17 19:58:00 -0600

    refactor () moved endpoints and keys to config file

---
+++ config/endpoints.js
@@ -0,0 +1,10 @@
+/**
+* @author Jack Considine <jackconsidine3@gmail.com>
+* @package fca1.0.0
+* 2017-11-16
+*/
+
+module.exports = {
+  YELP : "https://api.yelp.com/v3/businesses/",
+  YELLOW_PAGES : "http://api2.yp.com/listings/v1/search"
+}
