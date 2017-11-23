dd101a93fe3dbd78bd7c832b1ffae94ff50645a6
John P Considine <jconsidi@nd.edu>
2017-11-22 23:46:21 -0600

    chore () added google endpoints

--- config/endpoints.js
+++ config/endpoints.js
@@ -6,5 +6,7 @@
 
 module.exports = {
   YELP : "https://api.yelp.com/v3/businesses/",
-  YELLOW_PAGES : "http://api2.yp.com/listings/v1/search"
+  YELLOW_PAGES : "http://api2.yp.com/listings/v1/search",
+  GOOGLE : "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+  GOOGLE_MAPS : "https://maps.googleapis.com/maps/api/geocode/json"
 }
