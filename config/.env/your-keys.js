7c0fd4f988769507a0617bbe7d6fdf7488524bed
John P Considine <jconsidi@nd.edu>
2017-11-23 12:18:28 -0600

    chore () added example keys file for when repo is cloned

---
+++ config/.env/your-keys.js
@@ -0,0 +1,9 @@
+// Place your appropriate keys in the right spot
+
+module.exports = {
+  GOOGLE_PLACES_API_KEY : "",
+  GOOGLE_MAPS_API_KEY : "",
+  YELP_ID : "",
+  YELP_SECRET : "",
+  YELLOW_PAGES_API_KEY : ""
+}
