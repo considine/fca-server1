ba5b71f924ee51d5693f31031b7c2f6f360d63a2
John P Considine <jconsidi@nd.edu>
2017-11-22 23:45:27 -0600

    feat () added google places api

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,9 @@ var cors = require('cors')
 
 // Libs
 var PlacesNearbye = require("../lib/places-nearbye");
+var GooglePlaces = require("../lib/services/googleplaces-service");
+var GoogleApiKey = require("../config/.env/keys").GOOGLE_PLACES_API_KEY;
+var GoogleMaps = require("../config/.env/keys").GOOGLE_MAPS_API_KEY;
 
 router.get("/auto-repair/:latitude/:longitude/:limit", cors(), function(req, res) {
   PlacesNearbye.getAutoShops(req.params.latitude, req.params.longitude, req.params.limit).then((resp)=>{
@@ -24,6 +27,18 @@ router.get("/business", cors(), function(req, res) {
   .catch((e) => {
     res.status(500).send({"error" : true, "message" : e});
   });
+});
+
+
+
+router.get("/google-places/:zip", function(req, res) {
+  var gs = new GooglePlaces(GoogleApiKey, GoogleMaps);
+  gs.getPlaceIdsByZip(req.params.zip, "car_repair")
+    .then((results) => {
+      return res.send({"results" : results, "count" : results.length});
+    })
+    .catch((e) => res.status(400).send({"error" : true, "message" : e.message}));
+
 });
 
 
