8595796878b182788de523410b39b8c0471650ab
John P Considine <jconsidi@nd.edu>
2017-12-01 23:13:48 -0600

    chore () addded place details api URL

--- config/endpoints.js
+++ config/endpoints.js
@@ -8,5 +8,6 @@ module.exports = {
   YELP : "https://api.yelp.com/v3/businesses/",
   YELLOW_PAGES : "http://api2.yp.com/listings/v1/search",
   GOOGLE : "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+  GOOGLE_DETAIL : "https://maps.googleapis.com/maps/api/place/details/json",
   GOOGLE_MAPS : "https://maps.googleapis.com/maps/api/geocode/json"
 }
